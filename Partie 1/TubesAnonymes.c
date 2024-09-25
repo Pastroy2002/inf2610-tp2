c083523a336d8e80aab867dd26223cf5971b005b
Gabriel Cloutier <gabrielcloutier2002@gmail.com>
2024-09-25 11:23:02 -0400

    first commit

---
+++ Partie 1/TubesAnonymes.c
@@ -0,0 +1,60 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+void checkStatus(int status) {
+    status = WEXITSTATUS(status);
+    printf("With anonymous pipes, the cmp output is %d.", status);
+    if (status == 0) {
+        printf(" The compared texts are the same.\n");
+    } else if (status == 1) {
+        printf(" The compared texts are different.\n");
+    } else {
+        printf(" There was an error during the program's execution.\n");
+    }
+}
+
+int main() {
+    if (fork() == 0) { // P3
+        int fd1[2];
+        pipe(fd1);
+
+        if (fork() == 0) { // P2
+            close(fd1[0]);
+            int fd3[2];
+            pipe(fd3);
+
+            if (fork() == 0) { // P1
+                close(fd1[1]);
+                close(fd3[0]);
+                int textFile = open("In.txt", O_RDONLY);
+                dup2(textFile, 0);
+                close(textFile);
+                dup2(fd3[1],1);
+                close(fd3[1]);
+                execlp("rev", "rev", NULL);
+            }
+
+            close(fd3[1]);
+            wait(NULL);
+            dup2(fd3[0], 0);
+            close(fd3[0]);
+            dup2(fd1[1], 1);
+            close(fd1[1]);
+            execlp("rev", "rev", NULL);
+        }
+
+        close(fd1[1]);
+        while(wait(NULL) > 0);
+        
+        dup2(fd1[0], 0);
+        close(fd1[0]);
+        execlp("cmp", "cmp", "-", "In.txt", NULL);
+    }
+
+    int status;
+    while(wait(&status) > 0);
+    checkStatus(status);
+    return 0;
+}
---
+++ Partie 1/TubesNommes.c
@@ -0,0 +1,54 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+void checkStatus(int status) {
+    status = WEXITSTATUS(status);
+    printf("With named pipes, the cmp output is %d.", status);
+    if (status == 0) {
+        printf(" The compared texts are the same.\n");
+    } else if (status == 1) {
+        printf(" The compared texts are different.\n");
+    } else {
+        printf(" There was an error during the program's execution.\n");
+    }
+}
+
+int main() {
+    mkfifo("pipe1", 0600);
+    mkfifo("pipe2", 0600);
+    if (fork() == 0) { // P3
+        if (fork() == 0) { // P2
+            if (fork() == 0) { // P1
+                int fd = open("pipe1", O_WRONLY);
+                dup2(fd, 1);
+                close(fd);
+                int textFile = open("In.txt", O_RDONLY);
+                dup2(textFile, 0);
+                close(textFile);
+                execlp("rev", "rev", NULL);
+            }
+            int fd = open("pipe1", O_RDONLY);
+            wait(NULL);
+            dup2(fd, 0);
+            close(fd);
+            fd = open("pipe2", O_WRONLY);
+            dup2(fd, 1);
+            execlp("rev", "rev", NULL);
+        }
+        int fd = open("pipe2", O_RDONLY);
+        while(wait(NULL) > 0);
+        dup2(fd, 0);
+        close(fd);
+        execlp("cmp", "cmp", "-", "In.txt", NULL);
+    }
+    int status;
+    while(wait(&status) > 0);
+    checkStatus(status);
+    unlink("pipe1");
+    unlink("pipe2");
+    return 0;
+}
