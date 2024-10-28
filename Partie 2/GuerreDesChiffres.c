d4474672e314da4fba04cf7148a3ab38b0d18355
Gabriel Cloutier <gabrielcloutier2002@gmail.com>
2024-10-28 16:56:01 -0400

    working great. Added that the zeros count for consummed/produced. Cleaned up code

--- Partie 2/GuerreDesChiffres.c
+++ Partie 2/GuerreDesChiffres.c
@@ -16,36 +16,47 @@ int flag_de_fin = 0;
 int produced = 0;
 int consumed = 0;
 
+void produceNumber(int number) {
+    sem_wait(&prod);
+    sem_wait(&mutex);
+    int index = produced % sizeTamp;
+    tamp[index] = number;
+    produced++;
+    sem_post(&mutex);
+    sem_post(&cons);
+}
+
+int consumeNumber() {
+    sem_wait(&cons);
+    sem_wait(&mutex);
+    int index = consumed % sizeTamp;
+    int value = tamp[index];
+    consumed++;
+    sem_post(&mutex);
+    sem_post(&prod);
+    return value;
+}
+
 void* producteur(void* pid) {
     int* sum = malloc(sizeof(int)); 
     *sum = 0;
     while (flag_de_fin != 1) {
         int r = (rand ()%9) + 1;
         *sum += r;
-        sem_wait(&prod);
-        sem_wait(&mutex);
-        int index = produced % sizeTamp;
-        tamp[index] = r;
-        produced++;
-        sem_post(&mutex);
-        sem_post(&cons);
+        produceNumber(r);
     }
     pthread_exit((void*) sum);
 }
+
 
 void* consommateur(void* cid) {
     int* sum = malloc(sizeof(int)); 
     *sum = 0;
     while(1) {
-        sem_wait(&cons);
-        sem_wait(&mutex);
-        int index = consumed % sizeTamp;
-        int value = tamp[index];
-        if (value == 0) break;
+        int value = consumeNumber();
         *sum += value;
-        consumed++;
-        sem_post(&mutex);
-        sem_post(&prod);
+        if (value == 0) break;
+        
     }
     pthread_exit((void*) sum);
 }
@@ -54,23 +65,39 @@ void setToTrue(int sig) {
     flag_de_fin = 1;
 }
 
+void setUp() {
+    signal(SIGALRM, setToTrue);
+    srand(time(NULL));
+    
+    sem_init(&prod,0, sizeTamp);
+    sem_init(&cons, 0, 0);
+    sem_init(&mutex,0, 1);
+
+    tamp = (int*)malloc(sizeTamp*sizeof(int));
+}
+
+int getSum(int nbThreads, pthread_t threads[]) {
+    int sum = 0;
+    for (int i = 0; i < nbThreads; i++) {
+        int* value;
+        pthread_join(threads[i], (void**) &value);
+        sum += *value;
+        free(value);
+    }
+    return sum;
+}
+
 int main(int argc, char* argv[]) {
     // Les paramètres du programme sont, dans l'ordre :
     // le nombre de producteurs, le nombre de consommateurs
     // et la taille du tampon.
-    signal(SIGALRM, setToTrue);
-    srand(time(NULL));
+    
     const int nbProd = atoi(argv[1]);
     const int nbCons = atoi(argv[2]);
     sizeTamp = atoi(argv[3]);
-    sem_init(&prod,0, sizeTamp);
-    sem_init(&cons, 0, 0);
-    sem_init(&mutex,0, 1);
+    setUp();
     pthread_t consThread[nbCons];
     pthread_t prodThread[nbProd];
-    tamp = (int*)malloc(sizeTamp*sizeof(int));
-    int pid = 0;
-    int cid = 0;
 
     for (int i = 0; i < nbProd; i++) {
         pthread_create(&prodThread[i], NULL, producteur, (void*)i);
@@ -82,31 +109,14 @@ int main(int argc, char* argv[]) {
 
     alarm(1);
 
-    int prodSum = 0;
-    for (int i = 0; i < nbProd; i++) {
-        int* value;
-        pthread_join(prodThread[i], (void**) &value);
-        prodSum += *value;
-        free(value);
-    }
-
-    sem_wait(&mutex);
-    for (int i = 0; i < sizeTamp; i++) {
-        tamp[i] = 0;
-    }
+    int prodSum = getSum(nbProd, prodThread);
 
     for (int i = 0; i < nbCons; i++) {
-        sem_post(&cons);
-        sem_post(&mutex);
+        produceNumber(0);
     }
 
-    int consSum = 0;
-    for (int i = 0; i < nbCons; i++) {
-        int* value;
-        pthread_join(consThread[i], (void**) &value);
-        consSum += *value;
-        free(value);
-    }
+
+    int consSum = getSum(nbCons, consThread);
 
     free(tamp);
 
